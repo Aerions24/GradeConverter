c6568fa697d413a1a30cf4ccf9828c9283b6feec
Aerions24 <nicholasdbradley12@gmail.com>
2024-01-25 15:37:23 -0500

    testingchanges

--- src/App.java
+++ src/App.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        float grade;
+        char letter;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please input a grade between 0 and 100");
+        grade = scanner.nextFloat();
+
+        while (grade < 0 || grade > 100) {
+            System.out.println("Grade must be between 0 and 100. Please input again");
+            grade = scanner.nextFloat();
+        }
+
+        scanner.close();
+
+        // Converting  grade to letter with if elseif statements //
+        if (grade >= 90 && grade <= 100) {
+            letter = 'A';
+        } else if (grade >= 80 && grade < 90) {
+            letter = 'B';
+        } else if (grade >= 70 && grade < 80) {
+            letter = 'C';
+        } else if (grade >= 60 && grade < 70) {
+            letter = 'D';
+        } else {
+            letter = 'F';
+        }
+
+        System.out.println("Grade = " + grade);
+        System.out.println("Letter = " + letter);
     }
 }
+
